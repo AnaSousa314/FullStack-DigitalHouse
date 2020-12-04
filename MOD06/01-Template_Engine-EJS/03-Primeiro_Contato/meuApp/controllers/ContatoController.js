5a9fe9d0de31d25a60749c378c1e27e4cc2ae1ae
Ana.Sousa <anasousa314@outlook.com>
2020-12-04 18:15:57 -0300

    Atualiza o 03-Primeiro_Contato na pasta 01-Template_Engine-EJS no MOD06

--- MOD06/01-Template_Engine-EJS/03-Primeiro_Contato/meuApp/controllers/ContatoController.js
+++ MOD06/01-Template_Engine-EJS/03-Primeiro_Contato/meuApp/controllers/ContatoController.js
@@ -1,5 +1,8 @@
 const ContatoController = {
     index: (req,res)=>{
+
+        /* .render() é um método que se encontra dentro do objeto response da requisição. Nos permite enviar uma view ao navegador para que ele a renderize  */
+
         return res.render('contato');/* é o nome da view que está na pasta views. No entanto, por padrão não se coloca a extensão, assim contato.ejs. 
         Também não é necessário colocar o caminho onde se encontra, pq ele já está definido no app.js*/ 
     },
